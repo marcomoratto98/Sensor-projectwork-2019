5cb34eac076134b767d9649b05608868ac5b360b
Stefano Valle <s.valle@mvassociati.it>
2019-04-11 06:53:00 +0200

    Add DataSender project

---
+++ DataSender/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSRedis;
+
+namespace DataSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // configure Redis
+            var redis = new RedisClient("127.0.0.1");
+
+            while (true)
+            {
+                // read from Redis queue
+                Console.WriteLine(redis.BLPop(30, "sensors_data"));
+
+                // send value to remote API
+                // TODO...
+            }
+        }
+    }
+}
