2715dc54f58d99e7b009067ecd956026a849d65f
Stefano Valle <s.valle@mvassociati.it>
2019-04-11 06:34:34 +0200

    Esempio di sensore virtuale

--- DataReader/Program.cs
+++ DataReader/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataReader.Sensors;
+using CSRedis;
 
 namespace DataReader
 {
@@ -10,6 +12,23 @@ namespace DataReader
     {
         static void Main(string[] args)
         {
+            // init sensors
+            List<ISensor> sensors = new List<ISensor>
+            {
+                new VirtualTemperatureSensor()
+            };
+
+            while (true)
+            {
+                foreach (ISensor sensor in sensors)
+                {
+                    Console.WriteLine(sensor.ToJson());
+
+                    System.Threading.Thread.Sleep(1000);
+
+                }
+
+            }
         }
     }
 }
---
+++ DataReader/Sensors/VirtualTemperatureSensor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataReader.Sensors
+{
+    class VirtualTemperatureSensor : ITemperatureSensor, ISensor
+    {
+        public void SetTemperature(decimal temperature)
+        { }
+
+        public decimal GetTemperature()
+        {
+            Random random = new Random();
+            return new decimal((double)random.Next(10, 500) / 10);
+        }
+
+        public string ToJson()
+        {
+            return "{\"temperature\":\"" + GetTemperature() + "\"}";
+        }
+    }
+}
